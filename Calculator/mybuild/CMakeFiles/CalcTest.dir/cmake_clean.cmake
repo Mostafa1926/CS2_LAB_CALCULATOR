20ed5a521abfa302d98452511f14f0003d33cf14
Mohammad1789 <Mohammad_Dawood@aucegypt.edu>
2025-04-04 18:42:59 +0200

    CMakeLists added!

---
+++ Calculator/mybuild/CMakeFiles/CalcTest.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CalcTest.dir/Calculator.cpp.o"
+  "CMakeFiles/CalcTest.dir/Calculator.cpp.o.d"
+  "CMakeFiles/CalcTest.dir/test.cpp.o"
+  "CMakeFiles/CalcTest.dir/test.cpp.o.d"
+  "CalcTest"
+  "CalcTest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CalcTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
